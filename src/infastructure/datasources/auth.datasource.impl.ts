ef061fb3d09ec06f73fbf7c09a9c462ba0390c10
PonchoBT <ponchobt.dev@gmail.com>
2024-09-23 22:45:47 -0600

    added function hash

--- src/infastructure/datasources/auth.datasource.impl.ts
+++ src/infastructure/datasources/auth.datasource.impl.ts
@@ -3,7 +3,15 @@ import { UserModel } from '../../data/mongodb';
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from '../../domain';
 
 
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
+
 export class AuthDatasourceImpl implements AuthDatasource {
+
+  constructor(
+    private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+    private readonly comparePassword: CompareFunction = BcryptAdapter.compare,
+  ) {}
 
   async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
 
@@ -20,7 +28,7 @@ export class AuthDatasourceImpl implements AuthDatasource {
       const user = await UserModel.create({
         name: name,
         email: email,
-        password: BcryptAdapter.hash(password),
+        password: this.hashPassword( password ),
       });
 
       await user.save();
